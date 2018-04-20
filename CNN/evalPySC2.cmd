3aa0b3afc609ba2ae542f9fd71b08289d300b725
CrossR <sc13rjc@leeds.ac.uk>
2018-04-20 18:20:49 +0100

    Added quick shell script to ease getting evaluation numbers.

---
+++ CNN/evalPySC2.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+
+SET LOG_FILE=eval_logs/%mydate%_%mytime%_%MAP_NAME%_%MODEL_NAME%.log
+
+SET SCRIPT_TO_RUN=run.py
+SET MAP_NAME=MoveToBeacon
+SET MODEL_NAME=test_model_20
+SET SCRIPT_ARGS=--map_name %MAP_NAME% --model_name %MODEL_NAME% --training=False --if_output_exists continue
+
+type nul > %LOG_FILE%
+
+echo "Starting script..." > %LOG_FILE% 2>&1
+echo "Using %MODEL_NAME%, on map %MAP_NAME%." > %LOG_FILE% 2>&1
+python %SCRIPT_TO_RUN% %SCRIPT_ARGS% > %LOG_FILE% 2>&1
+echo "Finished script." > %LOG_FILE% 2>&1
