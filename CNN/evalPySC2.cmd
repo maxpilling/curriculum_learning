340de2818fa85951627922b63ac6ebae2ca45844
CrossR <sc13rjc@leeds.ac.uk>
2018-04-20 18:44:05 +0100

    Fixed script and added first performance metric.

--- CNN/evalPySC2.cmd
+++ CNN/evalPySC2.cmd
@@ -3,12 +3,12 @@
 For /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
-SET LOG_FILE=eval_logs/%mydate%_%mytime%_%MAP_NAME%_%MODEL_NAME%.log
-
 SET SCRIPT_TO_RUN=run.py
-SET MAP_NAME=MoveToBeacon
-SET MODEL_NAME=test_model_20
+SET MAP_NAME=CollectMineralShards
+SET MODEL_NAME=test_model_mineral
 SET SCRIPT_ARGS=--map_name %MAP_NAME% --model_name %MODEL_NAME% --training=False --if_output_exists continue
+
+SET LOG_FILE=eval_logs/%mydate%_%mytime%_%MAP_NAME%_%MODEL_NAME%.log
 
 type nul > %LOG_FILE%
 
